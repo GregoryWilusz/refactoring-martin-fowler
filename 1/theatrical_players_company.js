25e25d6f43c11e0f90b65164fa2d165a9e1e6216
Greg <wilusz.gregory@gmail.com>
2020-02-05 07:00:40 +0100

    Change function name (change function declaration)

--- 1/theatrical_players_company.js
+++ 1/theatrical_players_company.js
@@ -37,10 +37,10 @@ function volumeCreditsFor(aPerformance) {
   return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
     { style: "currency", currency: "USD",
-      minimumFractionDigits: 2}).format(aNumber);
+      minimumFractionDigits: 2}).format(aNumber/100);
 }
 
 function statement(invoice, plays) {
@@ -52,11 +52,11 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
   console.log(result);
