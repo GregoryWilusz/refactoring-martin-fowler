eb333560c6365933b941211c360c0b90526a69ad
Greg <wilusz.gregory@gmail.com>
2020-02-05 23:43:05 +0100

    Apply Move Function to totalVolumeCredits with updating references

--- 1/theatrical_players_company.js
+++ 1/theatrical_players_company.js
@@ -6,12 +6,21 @@ function statement(invoice, plays) {
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function totalAmount(data) {
     let result = 0;
     for (let perf of data.performances) {
       result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -66,17 +75,8 @@ function renderPlainText(data, plays) {
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  console.log(result);
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
@@ -87,4 +87,4 @@ function renderPlainText(data, plays) {
   }
 }
 
-statement(invoice[0], plays);
+console.log(statement(invoice[0], plays));
