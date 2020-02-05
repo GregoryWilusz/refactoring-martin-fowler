a6056c5fb872486e28d6ae9fcae704add632afff
Greg <wilusz.gregory@gmail.com>
2020-02-05 23:40:46 +0100

    Apply Move Function to totalAmount with updating references

--- 1/theatrical_players_company.js
+++ 1/theatrical_players_company.js
@@ -5,7 +5,16 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
   return renderPlainText(statementData, plays);
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -56,18 +65,10 @@ function renderPlainText(data, plays) {
   for (let perf of data.performances) {
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   console.log(result);
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
 
   function totalVolumeCredits() {
     let result = 0;
