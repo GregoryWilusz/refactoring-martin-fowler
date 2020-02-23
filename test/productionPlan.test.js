7b6f3e39e467131e17970f37b94478f995b952c3
Greg <wilusz.gregory@gmail.com>
2020-02-23 23:03:02 +0100

    Remove duplicated code from assertions

--- test/productionPlan.test.js
+++ test/productionPlan.test.js
@@ -2,13 +2,17 @@ const expect = require('chai').expect;
 const { Province, sampleProvinceData } = require('../4/productionPlan');
 
 describe('province', function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData);
+  });
+
+
   it('shortfall', function () {
-    let asia = new Province(sampleProvinceData);
     expect(asia.shortfall).equal(5);
   });
 
   it('profit', function () {
-    let asia = new Province(sampleProvinceData);
     expect(asia.profit).equal(230);
   });
 });
