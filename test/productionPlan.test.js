1b4ff45e159dfaa44482e43baa9100c4e8450b96
Greg <wilusz.gregory@gmail.com>
2020-02-23 22:58:08 +0100

    Add test for profit method

--- test/productionPlan.test.js
+++ test/productionPlan.test.js
@@ -6,5 +6,10 @@ describe('province', function () {
     let asia = new Province(sampleProvinceData);
     expect(asia.shortfall).equal(5);
   });
+
+  it('profit', function () {
+    let asia = new Province(sampleProvinceData);
+    expect(asia.profit).equal(230);
+  });
 });
 
