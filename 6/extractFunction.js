19d861fc931e040fd9535a8150185171486d93e1
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-01 23:55:46 +0100

    Extract function - extract printing details into nested function

--- 6/extractFunction.js
+++ 6/extractFunction.js
@@ -11,10 +11,14 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+
+  }
 }
 
 function printBanner() {
