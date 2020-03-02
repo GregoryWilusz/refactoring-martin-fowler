42bc1076f3fd806330ffa86187d05fc7517bfced
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-02 23:01:22 +0100

    Inline function - Move statement to callers #2

--- 6/inlineFunction.js
+++ 6/inlineFunction.js
@@ -1,10 +1,6 @@
 function reportLines(aCustomer) {
   const lines = [];
   out.push(["name", aCustomer.name]);
-  gatherCustomerData(lines, aCustomer);
-  return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
   out.push(["location", aCustomer.location]);
+  return lines;
 }
