76a01599039bb4d88ba26e945df118833fa49681
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-03 22:43:36 +0100

    Encapsulate Variable - replace reference with call to the getting function & assignment with setting function

--- 6/encapsulateVariable/encapsulateVariable.js
+++ 6/encapsulateVariable/encapsulateVariable.js
@@ -1,5 +1,5 @@
-let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
+spaceship.owner = getDefaultOwner();
+setDefaultOwner({ firstName: "Rebecca", lastName: "Parsons" });
 
-spaceship.owner = defaultOwner;
-
-defaultOwner = {firstName: "Rebecca", lastName: "Parsons"};
+function getDefaultOwner() { return defaultOwner; }
+function setDefaultOwner(arg) { defaultOwner = arg; }
