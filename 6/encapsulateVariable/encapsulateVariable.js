84b20fd9010539cbd02429f7e48250075574b11c
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-03 22:46:40 +0100

    Encapsulate Variable - move variable and accessors methods to their own file

--- 6/encapsulateVariable/encapsulateVariable.js
+++ 6/encapsulateVariable/encapsulateVariable.js
@@ -1,5 +1,2 @@
 spaceship.owner = getDefaultOwner();
-setDefaultOwner({ firstName: "Rebecca", lastName: "Parsons" });
 
-function getDefaultOwner() { return defaultOwner; }
-function setDefaultOwner(arg) { defaultOwner = arg; }
