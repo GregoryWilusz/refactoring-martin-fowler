0efd8f9ebf6e56bd518e1592e543c29ffaa9e8dc
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-04 00:44:34 +0100

    Combine Functions into Class - use Move Function to move calculateBaseCharge into a new class

--- 6/combineFunctionsIntoClass.js
+++ 6/combineFunctionsIntoClass.js
@@ -1,21 +1,18 @@
 const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 // client 1
-const aReading = acquireReading();
+const rawReading = acquireReading();
 const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
 
 // client 2
-const aReading = acquireReading();
+const rawReading = acquireReading();
 const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 
 // client 3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
-
-function calculateBaseCharge(aReading) {
-  return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const basicChargeAmount = aReading.calculateBaseCharge;
 
 class Reading { constructor(data) {
   this._customer = data.customer;
@@ -27,4 +24,8 @@ class Reading { constructor(data) {
   get quantity() {return this._quantity;}
   get month() {return this._month;}
   get year() {return this._year;}
+
+  get calculateBaseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
 }
