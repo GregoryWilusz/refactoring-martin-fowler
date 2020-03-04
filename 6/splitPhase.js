6d292bca073ca9f64c2b5ddd6c28ad2cf9573b5b
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-04 07:31:51 +0100

    Split Phase - introduce the intermediate data structure that will communicate between the two phases

--- 6/splitPhase.js
+++ 6/splitPhase.js
@@ -9,11 +9,12 @@ function priceOrder(product, quantity, shippingMethod) {
 function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = quantity * shippingPerCase;
-  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  const priceData = {};
+  const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
   return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = quantity * shippingPerCase;
   const price = basePrice - discount + shippingCost;
