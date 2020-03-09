564b0cb38cb90ecf720faddf5c11a909cd60e9c9
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-09 23:56:46 +0100

    Encapsulate record - replace users of the record with getters and setters

--- 7/encapsulateRecord.js
+++ 7/encapsulateRecord.js
@@ -2,14 +2,14 @@ class Organization {
   constructor(data) {
     this._data = data;
   }
+
+  set name(aString) {this._data.name = aString;}
+  get name() {return this._data.name;}
 }
 
 const organization = new Organization({ name: "Acme Gooseberries", country: "GB" });
-
-function getRawDataOfOrganization() {return organization._data;}
 function getOrganization() {return organization;}
 
+result += `<h1>${getOrganization().name}</h1>`;
 
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;
-
-getRawDataOfOrganization().name = newName;
+getOrganization().name = newName;
