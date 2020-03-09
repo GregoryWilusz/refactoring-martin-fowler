641cb75a962e2a2d394b2d9a42044fa151eca36f
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-09 23:51:51 +0100

    Encapsulate record - replace the record with a class

--- 7/encapsulateRecord.js
+++ 7/encapsulateRecord.js
@@ -1,5 +1,14 @@
-const organization = {name: "Acme Gooseberries", country: "GB"};
-function getRawDataOfOrganization() {return organization;}
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({ name: "Acme Gooseberries", country: "GB" });
+
+function getRawDataOfOrganization() {return organization._data;}
+function getOrganization() {return organization;}
+
 
 result += `<h1>${getRawDataOfOrganization().name}</h1>`;
 
