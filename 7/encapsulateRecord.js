49f7d3667260576aae056a086344d2a8d90e7980
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-09 23:46:40 +0100

    Encapsulate record - encapsulate variable

--- 7/encapsulateRecord.js
+++ 7/encapsulateRecord.js
@@ -1,6 +1,6 @@
 const organization = {name: "Acme Gooseberries", country: "GB"};
+function getRawDataOfOrganization() {return organization;}
 
-result += `<h1>${organization.name}</h1>`;
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
 
-organization.name = newName;
-
+getRawDataOfOrganization().name = newName;
