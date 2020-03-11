4489d09b99db5ec4247b96e786d717b6d8428305
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-11 07:30:37 +0100

    Extract Class - rename variables

--- 7/extractClass.js
+++ 7/extractClass.js
@@ -5,16 +5,16 @@ class Person {
   get name() {return this._name;}
   set name(arg) {this._name = arg;}
   get telephoneNumber() {return this._telephoneNumber.telephoneNumber;}
-  get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
-  set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode = arg;}
-  get officeNumber() {return this._telephoneNumber.officeNumber;}
-  set officeNumber(arg) {this._telephoneNumber.officeNumber = arg;}
+  get officeAreaCode() {return this._telephoneNumber.areaCode;}
+  set officeAreaCode(arg) {this._telephoneNumber.areaCode = arg;}
+  get officeNumber() {return this._telephoneNumber.number;}
+  set officeNumber(arg) {this._telephoneNumber.number = arg;}
 }
 
 class TelephoneNumber {
-  get officeAreaCode() {return this._officeAreaCode;}
-  set officeAreaCode(arg) {this._officeAreaCode = arg;}
-  get officeNumber() {return this._officeNumber;}
-  set officeNumber(arg) {this._officeNumber = arg;}
-  get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+  get areaCode() {return this.areaCode;}
+  set areaCode(arg) {this.areaCode = arg;}
+  get number() {return this.number;}
+  set number(arg) {this.number = arg;}
+  get telephoneNumber() {return `(${this.areaCode}) ${this.number}`;}
 }
