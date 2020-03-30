5096822c0d4cd61952e5ec37aab4a7c3dca6052b
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-30 23:47:08 +0200

    RenameField - defining a separate field and adjusting the constructor and accessors to use it.

--- 9/renameField.js
+++ 9/renameField.js
@@ -1,10 +1,24 @@
-class Organization { constructor(data) {
-  this._name = data.name;
-  this._country = data.country; }
-  get name() {return this._name;}
-  set name(aString) {this._name = aString;}
-  get country() {return this._country;}
-  set country(aCountryCode) {this._country = aCountryCode;}
+class Organization {
+  constructor(data) {
+    this._title = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._title;
+  }
+
+  set name(aString) {
+    this._title = aString;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
 }
 
-const organization = new Organization({name: "Acme Gooseberries", country: "GB"});
+const organization = new Organization({ name: "Acme Gooseberries", country: "GB" });
