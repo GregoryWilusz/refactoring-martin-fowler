b80227bc2e747d2ac268b7f52cf4c8fa82689d7b
GregoryWilusz <wilusz.gregory@gmail.com>
2020-03-31 21:45:23 +0200

    Change Reference to Value - make the Telephone Number immutable (Remove Setting Method)

--- 9/changeReferenceToValue.js
+++ 9/changeReferenceToValue.js
@@ -4,16 +4,20 @@ class Person {
   }
 
   get officeAreaCode() {return this._telephoneNumber.areaCode;}
+
   set officeAreaCode(arg) {this._telephoneNumber.areaCode = arg;}
 
   get officeNumber() {return this._telephoneNumber.number;}
+
   set officeNumber(arg) {this._telephoneNumber.number = arg;}
 }
 
 class TelephoneNumber {
-  get areaCode() {return this._areaCode;}
-  set areaCode(arg) {this._areaCode = arg;}
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
 
+  get areaCode() {return this._areaCode;}
   get number() {return this._number;}
-  set number(arg) {this._number = arg;}
 }
