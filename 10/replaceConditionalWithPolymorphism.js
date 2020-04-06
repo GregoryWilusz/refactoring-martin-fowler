cd6aafc2d0db6d743464ca6ef7eec712991b851e
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-06 23:27:01 +0200

    Replace Conditional with Polymorphism - add subclasses for each kind of bird, together with a factory function to instantiate the appropriate subclass

--- 10/replaceConditionalWithPolymorphism.js
+++ 10/replaceConditionalWithPolymorphism.js
@@ -19,27 +19,31 @@ class Bird {
     Object.assign(this, birdObject);
   }
   get plumage() {
-    switch (this.type) {
-      case 'EuropeanSwallow':
-        return "average";
-      case 'AfricanSwallow':
-        return (this.numberOfCoconuts > 2) ? "tired" : "average";
-      case 'NorwegianBlueParrot':
-        return (this.voltage > 100) ? "scorched" : "beautiful";
-      default:
-        return "unknown";
-    }
+    return createBird(bird).plumage;
   }
   get airSpeedVelocity() {
-    switch (this.type) {
-      case 'EuropeanSwallow':
-        return 35;
-      case 'AfricanSwallow':
-        return 40 - 2 * this.numberOfCoconuts;
-      case 'NorwegianBlueParrot':
-        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return createBird(bird).airSpeedVelocity;
   }
+}
+
+function createBird(bird) {
+  switch (bird.type) {
+    case 'EuropeanSwallow':
+      return new EuropeanSwallow(bird);
+    case 'AfricanSwallow':
+      return new AfricanSwallow(bird);
+    case 'NorweigianBlueParrot':
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+}
+
+class EuropeanSwallow extends Bird {
+}
+
+class AfricanSwallow extends Bird {
+}
+
+class NorwegianBlueParrot extends Bird {
 }
