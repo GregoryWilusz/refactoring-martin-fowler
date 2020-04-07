01076afa4c797b361cfda87997adb561403c5e81
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-07 21:49:46 +0200

    Replace Conditional with Polymorphism - override captainHistoryRisk method in the subclass

--- 10/usingPolymorphismForVariations.js
+++ 10/usingPolymorphismForVariations.js
@@ -42,7 +42,6 @@ class Rating {
     let result = 1;
     if (this.history.length < 5) result += 4;
     result += this.history.filter(v => v.profit < 0).length;
-    if (this.voyage.zone === "china" && this.hasChinaHistory) result -= 2;
     return Math.max(result, 0);
   }
 
@@ -68,4 +67,8 @@ class Rating {
 }
 
 class ExperiencedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
 }
