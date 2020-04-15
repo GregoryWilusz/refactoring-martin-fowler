50d9dade3f2a01d17178a98cc3a5da2db8a0503c
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-15 23:14:09 +0200

    Separate Query From Modifier - before refactoring

---
+++ 11/separateQueryFromModifier.js
@@ -0,0 +1,17 @@
+// Here is a function that scans a list of names for a miscreant.
+// If it finds one, it returns the name of the bad guy and sets off the alarms.
+// It only does this for the first miscreant it finds.
+
+function alertForMiscreant(people) {
+  for (const p of people) {
+    if (p === "Don") {
+      setOffAlarms();
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return "";
+}
