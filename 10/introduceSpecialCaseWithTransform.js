530cb0312034c4d25f2bb6e7232ce210959e5cc9
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-15 21:41:02 +0200

    Introduce Special Case (transform) - before refactoring

---
+++ 10/introduceSpecialCaseWithTransform.js
@@ -0,0 +1,42 @@
+// Let’s assume our input is a simple record structure that looks something like this:
+
+// {
+//   name: "Acme Boston",
+//   location: "Malden MA",
+//   // more site details
+//   customer: {
+//    name: "Acme Industries",
+//    billingPlan: "plan-451",
+//    paymentHistory: {
+//     weeksDelinquentInLastYear: 7
+//    //more
+//    },
+//    // more
+//   }
+// }
+
+// In some cases, the customer isn’t known, and such cases are marked in the same way:
+// {
+//   name: "Warehouse Unit 15",
+//   location: "Malden MA",
+//   // more site details
+//   customer: "unknown"
+// }
+
+// client 1
+  const site = acquireSiteData();
+  const aCustomer = site.customer;
+  // ... lots of intervening code ...
+  let customerName;
+  if (aCustomer === "unknown") customerName = "occupant";
+  else customerName = aCustomer.name;
+
+// client 2
+  const plan = (aCustomer === "unknown") ?
+  registry.billingPlans.basic
+  : aCustomer.billingPlan;
+
+// client 3
+  const weeksDelinquent = (aCustomer === "unknown") ?
+  0
+  : aCustomer.paymentHistory.weeksDelinquentInLastYear;
