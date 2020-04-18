b7e6cda59dcc1adaddd3cba9330b7e2cf42a7908
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-18 09:01:55 +0200

    Preserve Whole Object - rename function name to the original one

--- 11/preserveWholeObject.js
+++ 11/preserveWholeObject.js
@@ -2,11 +2,11 @@
 // daily temperature range with a range in a predefined heating plan.
 
 // caller...
-if (!aPlan.aPlan.xxNEWwithinRange(aRoom.daysTempRange))
+if (!aPlan.aPlan.withinRange(aRoom.daysTempRange))
   alerts.push("room temperature went outside range");
 
 class HeatingPlan {
-  xxNEWwithinRange(aNumberRange) {
+  withinRange(aNumberRange) {
     return (aNumberRange.low >= this._temperatureRange.low) &&
       (aNumberRange.high <= this._temperatureRange.high);
   }
