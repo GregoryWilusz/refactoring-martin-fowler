52f7e88512395069754c8761c6dd97ca6caa134d
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-18 14:24:19 +0200

    Replace Query With Parameter - add a clarification after refactoring

--- 11/replaceQueryWithParameter.js
+++ 11/replaceQueryWithParameter.js
@@ -1,6 +1,15 @@
 // It allows the user to select a temperature on a thermostat—but only
 // sets the target temperature within a range determined by a heating plan.
 
+
+// The HeatingPlan class is immutable — its fields are set in the constructor
+// with no methods to alter them. Given an immutable heating plan, by moving
+// the thermostat reference out of the function body I’ve also made
+// targetTemperature referentially transparent. Every time I call targetTemperature
+// on the same object, with the same argument, I will get the same result.
+// If all the methods of the heating plan have referential transparency,
+// that makes this class much easier to test and reason about.
+
 class HeatingPlan {
   targetTemperature(selectedTemperature) {
     if      (selectedTemperature > this._max) return this._max;
