b01b7d8f57d5cfc41bc8e531d14a1a173dcaa324
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-18 15:19:45 +0200

    Replace Query With Parameter - inline function to the setting method

--- 11/removeSettingMethod.js
+++ 11/removeSettingMethod.js
@@ -1,15 +1,12 @@
 class Person {
   constructor(id) {
-    this.id = id;
+    this._id = id;
   }
 
   get name() {return this._name;}
   set name(arg) {this._name = arg;}
   get id() {return this._id;}
-  set id(arg) {this._id = arg;}
 }
-
-// At the moment, I create a new object with code like this:
 
 const martin = new Person("1234");
 martin.name = "martin";
