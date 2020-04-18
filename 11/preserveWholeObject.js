c55585473547651a7c6116ca62132493ae57f3de
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-18 09:01:06 +0200

    Preserve Whole Object - inline function and remove dead code

--- 11/preserveWholeObject.js
+++ 11/preserveWholeObject.js
@@ -6,11 +6,8 @@ if (!aPlan.aPlan.xxNEWwithinRange(aRoom.daysTempRange))
   alerts.push("room temperature went outside range");
 
 class HeatingPlan {
-  withinRange(bottom, top) {
-    return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-  }
-
   xxNEWwithinRange(aNumberRange) {
-    return this.withinRange(aNumberRange.low, aNumberRange.high);
+    return (aNumberRange.low >= this._temperatureRange.low) &&
+      (aNumberRange.high <= this._temperatureRange.high);
   }
 }
