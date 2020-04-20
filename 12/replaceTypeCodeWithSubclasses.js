ea6f2513576f56b10eff4b498df9142baf2048ab
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-20 23:47:26 +0200

    Replace Type Code With Subclasses - Encapsulate Variable to self-encapsulate the type code

--- 12/replaceTypeCodeWithSubclasses.js
+++ 12/replaceTypeCodeWithSubclasses.js
@@ -8,5 +8,6 @@ class Employee {
     if (!["engineer", "manager", "salesman"].includes(arg))
       throw new Error(`Employee cannot be of type ${arg}`);
   }
-  toString() {return `${this._name} (${this._type})`;}
+  get type() {return this._type;}
+  toString() {return `${this._name} (${this.type})`;}
 }
