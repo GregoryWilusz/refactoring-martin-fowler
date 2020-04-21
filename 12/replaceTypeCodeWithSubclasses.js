83ffab76f8aea3ef44d88dae49da97d6b03665f3
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-21 22:41:09 +0200

    Replace Type Code With Subclasses - remove the type code field and the superclass getting method

--- 12/replaceTypeCodeWithSubclasses.js
+++ 12/replaceTypeCodeWithSubclasses.js
@@ -2,13 +2,11 @@ class Employee {
   constructor(name, type) {
     this.validateType(type);
     this._name = name;
-    this._type = type;
   }
   validateType(arg) {
     if (!["engineer", "manager", "salesman"].includes(arg))
       throw new Error(`Employee cannot be of type ${arg}`);
   }
-  get type() {return this._type;}
   toString() {return `${this._name} (${this.type})`;}
 }
 
