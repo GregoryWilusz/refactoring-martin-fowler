a482a3ad028ac0179e39ba2112b01847c42d4ddd
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-21 23:32:38 +0200

    Replace Type Code With Subclasses #2 - apply the usual mechanics of Replace Type Code with Subclasses to the employee type

--- 12/replaceTypeCodeWithSubclasses2.js
+++ 12/replaceTypeCodeWithSubclasses2.js
@@ -4,12 +4,19 @@
 // the type of employee.
 
 class EmployeeType {
-  constructor(aString) {
-    this._value = aString;
-  }
-  toString() {return this._value;}
 }
 
+class Engineer extends EmployeeType {
+  toString() {return "engineer";}
+}
+
+class Manager extends EmployeeType {
+  toString() {return "manager";}
+}
+
+class Salesman extends EmployeeType {
+  toString() {return "salesman";}
+}
 
 class Employee {
   constructor(name, type) {
@@ -22,7 +29,7 @@ class Employee {
       throw new Error(`Employee cannot be of type ${arg}`);
   }
   get typeString() {return this._type.toString();}
-  get type() {return this._type;}
+  set type(arg) {this._type = Employee.createEmployeeType(arg);}
   set type(arg) {this._type = arg;}
 
   get capitalizedType() {
@@ -31,5 +38,18 @@ class Employee {
   }
   toString() {
     return `${this._name} (${this.capitalizedType})`;
+  }
+
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer":
+        return new Engineer();
+      case "manager":
+        return new Manager();
+      case "salesman":
+        return new Salesman();
+      default:
+        throw new Error(`Employee cannot be of type ${aString}`);
+    }
   }
 }
