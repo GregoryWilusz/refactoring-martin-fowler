a685db8a2884e7466258dd92c21e2340f28c5158
GregoryWilusz <wilusz.gregory@gmail.com>
2020-04-25 15:37:09 +0200

    Replace Superclass with Delegate - create forwarding methods for each element of the superclass it is used on the subclass

--- 12/replaceSuperclassWithDeletage.js
+++ 12/replaceSuperclassWithDeletage.js
@@ -22,6 +22,12 @@ class Scroll extends CatalogItem {
     this._catalogItem = new CatalogItem(id, title, tags);
     this._lastCleaned = dateLastCleaned;
   }
+
+
+  get id() {return this._catalogItem.id;}
+  get title() {return this._catalogItem.title;}
+  hasTag(aString) {return this._catalogItem.hasTag(aString);}
+
   needsCleaning(targetDate) {
     const threshold = this.hasTag("revered") ? 700 : 1500;
     return this.daysSinceLastCleaning(targetDate) > threshold;
